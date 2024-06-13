277d7d79f0aadb81057411b1e46429164d82bb50
Michał Jaroch <azjaroch@wp.pl>
2024-06-13 22:32:44 +0200

    change color on text

---
+++ main.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector('h1')
+h1.addEventListener("click", () => {
+    if (h1.style.color == "red") {
+        h1.style.color = "black";
+        return;
+    }
+
+    h1.style.color = "red";
+})
